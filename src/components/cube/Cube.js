b141af4c75e59af6a8ff39574057ecbfbf2b1124
Arpan Srivastava <azs0239@auburn.edu>
2023-12-02 04:46:46 -0600

    scramble

--- src/components/cube/Cube.js
+++ src/components/cube/Cube.js
@@ -5,6 +5,13 @@ import { Face, Square, RowContainer, ColumnContainer, CubeContainer } from './Cu
 const Cube = ({inputCubeString}) => {
 
     console.log(separateColorsForEachFace(inputCubeString))
+    const leftFaceColors = separateColorsForEachFace(inputCubeString).left
+    const rightFaceColors = separateColorsForEachFace(inputCubeString).right
+    const frontFaceColors = separateColorsForEachFace(inputCubeString).front
+    const backFaceColors = separateColorsForEachFace(inputCubeString).back
+    const topFaceColors = separateColorsForEachFace(inputCubeString).top
+    const bottomFaceColors = separateColorsForEachFace(inputCubeString).bottom
+    
 
 
   return (
@@ -12,72 +19,72 @@ const Cube = ({inputCubeString}) => {
         
         <RowContainer>
             <Face id="left">
-                <Square colorOfPiece={"red"}/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
-                <Square colorOfPiece="red"/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[0] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[1] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[2] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[3] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[4] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[5] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[6] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[7] : "red"}/>
+                <Square colorOfPiece={leftFaceColors.length !== 0 ? leftFaceColors[8] : "red"}/>
             </Face>
             <ColumnContainer>
                 <Face id="top">
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
-                    <Square colorOfPiece="yellow"/>
+                    <Square colorOfPiece= {topFaceColors.length !== 0 ? topFaceColors[0] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[1] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[2] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[3] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[4] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[5] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[6] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[7] : "yellow"}/>
+                    <Square colorOfPiece={topFaceColors.length !== 0 ? topFaceColors[8] : "yellow"}/>
                 </Face>
                 <Face id="front">
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
-                    <Square colorOfPiece="green"/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[0] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[1] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[2] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[3] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[4] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[5] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[6] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[7] : "green" }/>
+                    <Square colorOfPiece={frontFaceColors.length !== 0 ? frontFaceColors[8] : "green" }/>
                 </Face>
                 <Face id="bottom">
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
-                    <Square colorOfPiece="white"/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[0] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[1] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[2] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[3] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[4] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[5] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[6] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[7] : "white"}/>
+                    <Square colorOfPiece={bottomFaceColors.length !== 0 ? bottomFaceColors[8] : "white"}/>
                 </Face>
             </ColumnContainer>
             <Face id="right">
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
-                <Square colorOfPiece="orange"/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[0] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[1] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[2] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[3] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[4] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[5] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[6] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[7] : "orange"}/>
+                <Square colorOfPiece={rightFaceColors.length !== 0 ? rightFaceColors[8] : "orange"}/>
             </Face>
             <Face id="back">
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
-                <Square colorOfPiece="blue"/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[0] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[1] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[2] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[3] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[4] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[5] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[6] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[7] : "blue"}/>
+                <Square colorOfPiece={backFaceColors.length !== 0 ? backFaceColors[8] : "blue"}/>
             </Face>            
         </RowContainer>
         
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     setCubeString(event.target.value)
   }
   
-  const getResponse = async () => {
+  const getSolveResponse = async () => {
     if (cubeString.length !== validCubeStringLength){
       alert('Cube string must be 54 characters long')
       return
@@ -28,13 +28,51 @@ const Home = () => {
     }
 
     try{
-      const response = await api.get('/rubik/solve?cube=' + cubeString)
+      const response = await api.get(`/rubik/solve?cube=${cubeString}`)
       console.log(response)
       parseResponse(response.data)
     }
     catch(error){
       console.log(error)
     }
+  }
+
+  const getScrambleResponse = async () => {
+
+    //TODO: This is getting messy, I should probably make a function to handle this
+    if (cubeString.length !== validCubeStringLength){
+      alert('Cube string must be 54 characters long')
+      return
+    }
+
+    if (regex.test(cubeString)) {
+        alert("Cube string must only contain 'r', 'b', 'g', 'o', 'y', 'w'")
+        return
+    }
+
+    try{
+      const response = await api.get(`/rubik/rotate?dir=${randomRotations()}&cube=${cubeString}`)
+      console.log(response)
+      response.data = response.data.split(' ')
+      response.data = response.data[1].replace(',', '').replace(/'/g, '')
+      setCubeString(response.data)
+      console.log(response.data)
+      
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  const randomRotations = () => {
+    const rotations = ['F', 'f', 'B', 'b', 'R', 'r', 'L', 'l', 'U', 'u']
+    let scramble = ''
+    for (let i = 0; i < 4; i++){
+      scramble += rotations[Math.floor(Math.random() * rotations.length)]
+
+    }
+    console.log(scramble)
+    return scramble
   }
 
   const parseResponse = (response) => {
@@ -48,18 +86,18 @@ const Home = () => {
     return(
       setCubeSolution(response[1].replace(',', '').replace(/'/g, ''))
     )
-    }
+  }
   return (
     <div className='homeContainer'>
       <p>{regex.test(cubeString)&& "Cube string must only contain 'r', 'b', 'g', 'o', 'y', 'w'"}</p>
       <div className='cubeStringFields'>
         
-        <input placeholder='Enter input Cube' className='userInputField' type="text" onChange={handleChange} defaultValue={'gggggggggbbbbbbbbbrrrrrrrrroooooooooyyyyyyyyywwwwwwwww'}/>
+        <input placeholder='Enter input Cube' className='userInputField' type="text" onChange={handleChange} defaultValue={'gggggggggbbbbbbbbbrrrrrrrrroooooooooyyyyyyyyywwwwwwwww'} value={cubeString}/>
         
       </div>
       <div className="userButtons responsive">
-        <button onClick={getResponse} >Solve</button>
-        <button onClick={getResponse} >Scramble</button>
+        <button onClick={getSolveResponse} >Solve</button>
+        <button onClick={getScrambleResponse} >Scramble</button>
       </div>
       <Cube inputCubeString={cubeString.length === validCubeStringLength && !(regex.test(cubeString)) ? cubeString : ""}/>
 
