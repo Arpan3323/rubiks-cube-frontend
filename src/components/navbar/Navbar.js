616d28f3da0e8bc2702de5365c12f1f9d73e33f3
Arpan Srivastava <azs0239@auburn.edu>
2024-01-10 14:25:35 -0600

    navbar update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <a href='/' className='siteTitle'>Rubik's Cube Solver</a>
+        <a href='/rubiks-cube-frontend/' className='siteTitle'>Rubik's Cube Solver</a>
         <div className='sourceLinks'>
           <a  href='https://github.com/Arpan3323/Rubiks-Cube-Solver' target='_blank' rel="noreferrer">Back-End Source Code</a>
           <a  href='https://github.com/Arpan3323/rubiks-cube-frontend' target='_blank' rel="noreferrer">Front-End Source Code</a>
